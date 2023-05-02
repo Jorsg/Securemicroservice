9e81306daee96201eea570ce07673cef06090e87
Jorman Garcia <jagarcia@lfs-inc.com>
2023-05-01 22:23:44 -0400

    Firts commit, create class movies, folder, Data, Model and Controller

---
+++ Movies.API/Data/MoviesContextSeed.cs
@@ -0,0 +1,98 @@
+﻿using Movies.API.Model;
+
+namespace Movies.API.Data
+{
+	public class MoviesContextSeed
+	{
+		public static void SeedAsync(MoviesContext moviesContext)
+		{
+			if (!moviesContext.Movies.Any())
+			{
+				var movies = new List<Model.Movies>
+				{
+					new Model.Movies {
+						 Id = 1,
+						Genre = "Drama",
+						Title = "The Shawshank Redemption",
+						Rating = "9.3",
+						ImageUrl = "images/src",
+						ReleaseDate = new DateTime(1994, 5, 5),
+						Owner = "alice"
+					},
+					new Model.Movies
+					{
+						Id = 2,
+						Genre = "Crime",
+						Title = "The Godfather",
+						Rating = "9.2",
+						ImageUrl = "images/src",
+						ReleaseDate = new DateTime(1972, 5, 5),
+						Owner = "alice"
+					},
+					new Model.Movies
+					{
+						Id = 3,
+						Genre = "Action",
+						Title = "The Dark Knight",
+						Rating = "9.1",
+						ImageUrl = "images/src",
+						ReleaseDate = new DateTime(2008, 5, 5),
+						Owner = "bob"
+					},
+					new Model.Movies
+					{
+						Id = 4,
+						Genre = "Crime",
+						Title = "12 Angry Men",
+						Rating = "8.9",
+						ImageUrl = "images/src",
+						ReleaseDate = new DateTime(1957, 5, 5),
+						Owner = "bob"
+					},
+					new Model.Movies
+					{
+						Id = 5,
+						Genre = "Biography",
+						Title = "Schindler's List",
+						Rating = "8.9",
+						ImageUrl = "images/src",
+						ReleaseDate = new DateTime(1993, 5, 5),
+						Owner = "alice"
+					},
+					new Model.Movies
+					{
+						Id = 6,
+						Genre = "Drama",
+						Title = "Pulp Fiction",
+						Rating = "8.9",
+						ImageUrl = "images/src",
+						ReleaseDate = new DateTime(1994, 5, 5),
+						Owner = "alice"
+					},
+					new Model.Movies
+					{
+						Id = 7,
+						Genre = "Drama",
+						Title = "Fight Club",
+						Rating = "8.8",
+						ImageUrl = "images/src",
+						ReleaseDate = new DateTime(1999, 5, 5),
+						Owner = "bob"
+					},
+					new Model.Movies
+					{
+						Id = 8,
+						Genre = "Romance",
+						Title = "Forrest Gump",
+						Rating = "8.8",
+						ImageUrl = "images/src",
+						ReleaseDate = new DateTime(1994, 5, 5),
+						Owner = "bob"
+					}
+				};
+				moviesContext.Movies.AddRange(movies);
+				moviesContext.SaveChanges();				
+			}
+		}
+	}
+}
