3fcedd083f7d205ef12ce1690220443b8f87df4d
Jorman Garcia <jagarcia@lfs-inc.com>
2023-05-04 11:47:18 -0400

    Create DataBase Inmemory to swagger

--- Movies.API/Program.cs
+++ Movies.API/Program.cs
@@ -1,27 +1,50 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Movies.API.Data;
-var builder = WebApplication.CreateBuilder(args);
-builder.Services.AddDbContext<MoviesContext>(options =>
-    options.UseInMemoryDatabase(("Movies")));
+using Microsoft.Extensions.Hosting;
+using Movies.API;
+using Microsoft.AspNetCore.Hosting;
 
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddControllers();
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+public class Program
 {
-	app.UseSwagger();
-	app.UseSwaggerUI();
+	public static void Main(string[] args)
+	{
+		var builder = WebApplication.CreateBuilder(args);
+
+
+		// Add services to the container.
+		// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+		builder.Services.AddControllers();
+		builder.Services.AddEndpointsApiExplorer();
+		builder.Services.AddSwaggerGen();
+		builder.Services.AddDbContext<MoviesContext>(options =>
+					options.UseInMemoryDatabase(("Movies")));
+
+		var configBuilder = new ConfigurationBuilder()
+			.AddEnvironmentVariables()
+			.AddJsonFile("appsetting.json");
+
+
+		var app = builder.Build();
+
+		// Configure the HTTP request pipeline.
+		if (app.Environment.IsDevelopment())
+		{
+			app.UseSwagger();
+			app.UseSwaggerUI();
+		}
+
+		app.UseHttpsRedirection();
+		app.UseAuthorization();
+		app.MapControllers();
+		// Seed the database
+		using (var scope = app.Services.CreateScope())
+		{
+			var serviceProvider = scope.ServiceProvider;
+			var moviesContext = serviceProvider.GetRequiredService<MoviesContext>();
+			MoviesContextSeed.SeedAsync(moviesContext);
+		}
+		app.Run();
+	}
+
 }
-
-
-app.UseHttpsRedirection();
-app.UseAuthorization();
-app.MapControllers();
-app.Run();
