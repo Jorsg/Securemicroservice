92ea4384773def23c6de7131cad377ceeb1de6f4
Jorman Garcia <jagarcia@lfs-inc.com>
2023-05-05 15:33:07 -0400

    Building API IdentityServer4 Authentication Microservices

---
+++ IdentityServer/Config.cs
@@ -0,0 +1,44 @@
+﻿using IdentityServer4.Models;
+using IdentityServer4.Test;
+
+namespace IdentityServer
+{
+	public class Config
+	{
+		public static IEnumerable<Client> Clients =>
+			new Client[]
+			{
+				new Client
+				{
+					ClientId = "moviClient",
+					AllowedGrantTypes = GrantTypes.ClientCredentials,
+					ClientSecrets =
+					{
+						new Secret("secrect".Sha256())
+					},
+					AllowedScopes = {"moviesAPI"}
+				}
+			};
+
+		public static IEnumerable<ApiScope> ApiScopes =>
+			new ApiScope[]
+			{
+
+			};
+
+		public static IEnumerable<ApiResource> ApiResources =>
+			new ApiResource[]
+			{
+			};
+
+		public static IEnumerable<IdentityResource> identityResources =>
+			new IdentityResource[]
+			{
+			};
+
+		public static List<TestUser> TestUsers =>
+			new List<TestUser>
+			{
+			};
+	}
+}
