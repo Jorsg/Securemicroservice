e6289bb50c4273dba7f23088fc7f747f43474eeb
Jorman Garcia <jagarcia@lfs-inc.com>
2023-05-11 20:08:55 -0400

    Building MVC Client application for consuming API Resource - Movie.Client App

---
+++ Movies.Client/ApiServices/MovieApiService.cs
@@ -0,0 +1,54 @@
+﻿using Movies.Client.Models;
+
+namespace Movies.Client.ApiServices
+{
+	public class MovieApiService : IMovieApiService
+	{
+		public Task<Movie> CreateMovie(Movie movie)
+		{
+			throw new NotImplementedException();
+		}
+
+		public Task DeleteMovie(int id)
+		{
+			throw new NotImplementedException();
+		}
+
+		public Task<Movie> GetMovie(string id)
+		{
+			throw new NotImplementedException();
+		}
+
+		public async Task<IEnumerable<Movie>> GetMovies()
+		{
+			var movieList = new List<Movie>();
+			movieList.Add(new Movie
+			{
+				Id = 1,
+				Genre = "Comics",
+				Title = "Fantasy",
+				ImageUrl = "img/src",
+				Rating = "5.6",
+				ReleaseDate = DateTime.Now,
+				Owner = "jho"
+			});
+			movieList.Add(new Movie
+			{
+				Id = 2,
+				Genre = "Drama",
+				Title = "Queen",
+				ImageUrl = "img/src",
+				Rating = "8.6",
+				ReleaseDate = DateTime.Now,
+				Owner = "jho"
+			});
+
+			return await Task.FromResult(movieList);
+		}
+
+		public Task<Movie> UpdateMovie(Movie movie)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
