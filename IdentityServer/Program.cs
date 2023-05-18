b89d45633222e9b1cfcf249633d06d18813c2c75
Jorman Garcia <jagarcia@lfs-inc.com>
2023-05-17 22:26:30 -0400

    OpenId Connet with IdentityServer4 for interactive MVC

--- IdentityServer/Program.cs
+++ IdentityServer/Program.cs
@@ -6,14 +6,16 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+//builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddControllersWithViews();
+//builder.Services.AddSwaggerGen();
+
+
 builder.Services.AddIdentityServer()
 	.AddInMemoryClients(Config.Clients)
-	//.AddInMemoryIdentityResources(Config.identityResources)
-	//.AddInMemoryApiResources(Config.ApiResources)
 	.AddInMemoryApiScopes(Config.ApiScopes)
-	//.AddTestUsers(Config.TestUsers)
+	.AddInMemoryIdentityResources(Config.identityResources)
+	.AddTestUsers(Config.TestUsers)
 	.AddDeveloperSigningCredential();
 
 var app = builder.Build();
@@ -21,13 +23,21 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-	app.UseSwagger();
-	app.UseSwaggerUI();
+	//app.UseSwagger();
+	//app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseRouting();
 app.UseIdentityServer();
+app.UseAuthorization();
 
-
+app.UseEndpoints(endpoints =>
+{
+	app.UseEndpoints(endpoints =>
+	{
+		endpoints.MapDefaultControllerRoute();
+	});
+});
 
 app.Run();
