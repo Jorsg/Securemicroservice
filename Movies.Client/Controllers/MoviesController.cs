92c83313e35cf5bef01704f747f22de2cb809aa8
Jorman Garcia <jagarcia@lfs-inc.com>
2023-05-22 12:59:45 -0400

    IdentityServer4 OpenId Integration For Movie.Client MVC interactive Client App

--- Movies.Client/Controllers/MoviesController.cs
+++ Movies.Client/Controllers/MoviesController.cs
@@ -1,14 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
+using System.Security.Permissions;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.IdentityModel.Protocols.OpenIdConnect;
 using Movies.Client.ApiServices;
 using Movies.Client.Models;
 
 namespace Movies.Client.Controllers
 {
+    [Authorize]
     public class MoviesController : Controller
     {
         private readonly IMovieApiService _movieApiService;
@@ -22,8 +30,26 @@ namespace Movies.Client.Controllers
 		// GET: Movies
 		public async Task<IActionResult> Index()
         {
+            LogTokenAndClaim();
             return View(await _movieApiService.GetMovies());
         }
+
+        public async Task Logout()
+        {
+            await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+            await HttpContext.SignOutAsync(OpenIdConnectDefaults.AuthenticationScheme);
+        }
+
+        public async Task LogTokenAndClaim()
+        {
+            var identityToken = await HttpContext.GetTokenAsync(OpenIdConnectParameterNames.IdToken);
+            Debug.WriteLine($"Identity token: {identityToken}");
+            foreach (var claim in User.Claims)
+            {
+                Debug.WriteLine($"Claim type: {claim.Type} - {claim.Value}");
+            }
+        }
+
 
         // GET: Movies/Details/5
         public async Task<IActionResult> Details(string id)
--- Movies.Client/Program.cs
+++ Movies.Client/Program.cs
@@ -1,11 +1,36 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.Extensions.DependencyInjection;
 using Movies.Client.ApiServices;
 
 var builder = WebApplication.CreateBuilder(args);
+var config = builder.Configuration;
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 builder.Services.AddScoped<IMovieApiService, MovieApiService>();
+
+builder.Services.AddAuthentication(opt =>
+{
+	opt.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+	opt.DefaultChallengeScheme = OpenIdConnectDefaults.AuthenticationScheme;
+})
+	.AddCookie(CookieAuthenticationDefaults.AuthenticationScheme)
+	.AddOpenIdConnect(OpenIdConnectDefaults.AuthenticationScheme, option =>
+	{
+		//option.Authority = config["Authority:Url"];//
+		option.Authority = "https://localhost:5005";
+
+		option.ClientId = "movies_mvc_client";
+		option.ClientSecret = "secret";
+		option.ResponseType = "code";
+
+		option.Scope.Add("openid");
+		option.Scope.Add("profile");
+
+		option.SaveTokens = true;
+		option.GetClaimsFromUserInfoEndpoint = true;
+	});
 
 var app = builder.Build();
 
@@ -22,6 +47,7 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllerRoute(
