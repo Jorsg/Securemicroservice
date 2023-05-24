8cecf9e59a3c09e557d850ba3a316c71f31ea3b0
Jorman Garcia <jagarcia@lfs-inc.com>
2023-05-23 22:16:49 -0400

    Consume Protected Movie.API With HttpClientFactory From Movie.Client

--- Movies.Client/Program.cs
+++ Movies.Client/Program.cs
@@ -1,7 +1,10 @@
-﻿using Microsoft.AspNetCore.Authentication.Cookies;
+﻿using IdentityModel.Client;
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Authentication.OpenIdConnect;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Net.Http.Headers;
 using Movies.Client.ApiServices;
+using Movies.Client.HttpHandlers;
 
 var builder = WebApplication.CreateBuilder(args);
 var config = builder.Configuration;
@@ -18,8 +21,7 @@ builder.Services.AddAuthentication(opt =>
 	.AddCookie(CookieAuthenticationDefaults.AuthenticationScheme)
 	.AddOpenIdConnect(OpenIdConnectDefaults.AuthenticationScheme, option =>
 	{
-		//option.Authority = config["Authority:Url"];//
-		option.Authority = "https://localhost:5005";
+		option.Authority = config["Authority:Url"];		
 
 		option.ClientId = "movies_mvc_client";
 		option.ClientSecret = "secret";
@@ -31,6 +33,33 @@ builder.Services.AddAuthentication(opt =>
 		option.SaveTokens = true;
 		option.GetClaimsFromUserInfoEndpoint = true;
 	});
+
+builder.Services.AddTransient<AuthenticationDelegatingHandler>();
+
+builder.Services.AddHttpClient("MovieAPIClient", client =>
+{
+	client.BaseAddress = new Uri(config["Movie_App:Url"]);
+	client.DefaultRequestHeaders.Clear();
+	client.DefaultRequestHeaders.Add(HeaderNames.Accept, "application/json");
+
+}).AddHttpMessageHandler<AuthenticationDelegatingHandler>();
+
+//2 Create an HttpClient used for accesing the IDP
+
+builder.Services.AddHttpClient("IDPClient", client =>
+{
+	client.BaseAddress = new Uri(config["Authority:Url"]);
+client.DefaultRequestHeaders.Clear();
+client.DefaultRequestHeaders.Add(HeaderNames.Accept, "application/json");
+});
+
+builder.Services.AddSingleton(new ClientCredentialsTokenRequest
+{
+	Address = string.Concat(config["Authority:Url"], "/connect/token"),
+	ClientId = "movieClient",
+	ClientSecret = "secret",
+	Scope = "movieAPI"
+});
 
 var app = builder.Build();
 
