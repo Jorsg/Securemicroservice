eb58e3cdd8b49e48d7bd6c2c3733e967808ee21e
Jorman Garcia <jagarcia@lfs-inc.com>
2023-05-23 22:17:49 -0400

    Update program file app Movies.Client

--- Movies.Client/Program.cs
+++ Movies.Client/Program.cs
@@ -15,32 +15,32 @@ builder.Services.AddScoped<IMovieApiService, MovieApiService>();
 
 builder.Services.AddAuthentication(opt =>
 {
-	opt.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
-	opt.DefaultChallengeScheme = OpenIdConnectDefaults.AuthenticationScheme;
+    opt.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+    opt.DefaultChallengeScheme = OpenIdConnectDefaults.AuthenticationScheme;
 })
-	.AddCookie(CookieAuthenticationDefaults.AuthenticationScheme)
-	.AddOpenIdConnect(OpenIdConnectDefaults.AuthenticationScheme, option =>
-	{
-		option.Authority = config["Authority:Url"];		
+    .AddCookie(CookieAuthenticationDefaults.AuthenticationScheme)
+    .AddOpenIdConnect(OpenIdConnectDefaults.AuthenticationScheme, option =>
+    {
+        option.Authority = config["Authority:Url"];
 
-		option.ClientId = "movies_mvc_client";
-		option.ClientSecret = "secret";
-		option.ResponseType = "code";
+        option.ClientId = "movies_mvc_client";
+        option.ClientSecret = "secret";
+        option.ResponseType = "code";
 
-		option.Scope.Add("openid");
-		option.Scope.Add("profile");
+        option.Scope.Add("openid");
+        option.Scope.Add("profile");
 
-		option.SaveTokens = true;
-		option.GetClaimsFromUserInfoEndpoint = true;
-	});
+        option.SaveTokens = true;
+        option.GetClaimsFromUserInfoEndpoint = true;
+    });
 
 builder.Services.AddTransient<AuthenticationDelegatingHandler>();
 
 builder.Services.AddHttpClient("MovieAPIClient", client =>
 {
-	client.BaseAddress = new Uri(config["Movie_App:Url"]);
-	client.DefaultRequestHeaders.Clear();
-	client.DefaultRequestHeaders.Add(HeaderNames.Accept, "application/json");
+    client.BaseAddress = new Uri(config["Movie_App:Url"]);
+    client.DefaultRequestHeaders.Clear();
+    client.DefaultRequestHeaders.Add(HeaderNames.Accept, "application/json");
 
 }).AddHttpMessageHandler<AuthenticationDelegatingHandler>();
 
@@ -48,17 +48,17 @@ builder.Services.AddHttpClient("MovieAPIClient", client =>
 
 builder.Services.AddHttpClient("IDPClient", client =>
 {
-	client.BaseAddress = new Uri(config["Authority:Url"]);
-client.DefaultRequestHeaders.Clear();
-client.DefaultRequestHeaders.Add(HeaderNames.Accept, "application/json");
+    client.BaseAddress = new Uri(config["Authority:Url"]);
+    client.DefaultRequestHeaders.Clear();
+    client.DefaultRequestHeaders.Add(HeaderNames.Accept, "application/json");
 });
 
 builder.Services.AddSingleton(new ClientCredentialsTokenRequest
 {
-	Address = string.Concat(config["Authority:Url"], "/connect/token"),
-	ClientId = "movieClient",
-	ClientSecret = "secret",
-	Scope = "movieAPI"
+    Address = string.Concat(config["Authority:Url"], "/connect/token"),
+    ClientId = "movieClient",
+    ClientSecret = "secret",
+    Scope = "movieAPI"
 });
 
 var app = builder.Build();
@@ -66,9 +66,9 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-	app.UseExceptionHandler("/Home/Error");
-	// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-	app.UseHsts();
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
 }
 
 app.UseHttpsRedirection();
@@ -80,7 +80,7 @@ app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllerRoute(
-	name: "default",
-	pattern: "{controller=Home}/{action=Index}/{id?}");
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
